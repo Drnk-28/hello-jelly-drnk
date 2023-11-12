baf1d9bb173281810205e0c7e855ed4c2ace0c5c
drnk.28 <fuadfuuk12345@gmail.com>
2023-11-12 23:43:38 +0700

    Update index.js
    Signed-off-by: drnk.28 <fuadfuuk12345@gmail.com>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")()
 const port = 3000
 
 app.get("/", async (req, res) => {
-  res.send("hi")
+  res.send("<h1>heyo</h1><br><h2>heyo</h2>")
 })
 
 app.listen(port, () => {
